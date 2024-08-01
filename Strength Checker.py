63e9ce94d926da6465e5e1d6c5d1c3ae95b41919
scyan00ffff <99845681+scyan00ffff@users.noreply.github.com>
2024-08-01 04:10:45 +0100

    added password generator

--- Strength Checker.py
+++ Strength Checker.py
@@ -1,16 +1,17 @@
 from threading import Thread
 from multiprocessing import Queue 
 from queue import Empty
-import time 
+import random
+import string
 
 numbers = ["1","2","3","4","5","6","7","8","9"]
 special_characters = ["!","@","$","£","%","^","&","*","(",")","_","-","+","_","[","]","{","}","<",">","/","?",";",":"]
 characters = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"]
 
-while True:
+def strength_checker():
     user_input = input("Enter Password: ")
-    score = 0
 
+    score = 0
 
     for x in range(0, len(special_characters)):
         if special_characters[x] in user_input:
@@ -28,7 +29,7 @@ while True:
 
 
     if score == 0:
-        print(" Very Weak Password")
+        print("Very Weak Password")
     elif score >= 1 and score < 3:
         print("Weak Password")
     elif score >= 3 and score < 5:
@@ -52,14 +53,55 @@ while True:
 
 
 
+def password_generator():
+    character_list = ""
+    #eventually these inputs will be turned into check boxes on the GUI
+    use_special_character = input("Do you want to use special characters? (Y/N): ")
+    use_uppercase_character = input("Do you want to use uppercase characters? (Y/N): ")
+    use_numbers_character = input("Do you want to use numbers? (Y/N): ")
+    length_of_password = int(input("Enter the desired length of your password: "))
+
+    password = []
+
+    if use_special_character == "N" and use_numbers_character == "N" and use_uppercase_character == "N":
+        character_list += string.ascii_letters
+    elif use_special_character == "Y" and use_numbers_character == "N" and use_uppercase_character == "N":
+        character_list += string.ascii_letters + string.punctuation
+    elif use_special_character == "Y" and use_numbers_character == "Y" and use_uppercase_character == "N":
+        character_list += string.ascii_letters + string.punctuation + string.digits
+    elif use_special_character == "Y" and use_numbers_character == "Y" and use_uppercase_character == "Y":
+        character_list += string.ascii_letters + string.punctuation + string.ascii_uppercase + string.digits
+        
+
+    for desired_length in range(length_of_password):
+        randomchar = random.choice(character_list)
+        password.append(randomchar)
+    print("Your password is: " + "".join(password))
+
+        
+
+while True:
+    print("Select which tool you wish to use: ")
+    selection = input("Password Generator (1) Password Checker (2): ")
+
+    if selection == "2":
+        strength_checker()
+
+        
+    elif selection == "1":
+        password_generator()
+
+
+
+
+
+
 
 
 
 
 
 # To do
-# - Add common passwords database/breached passwords
-# - Add password generator
 # - Add Gui
 
 
